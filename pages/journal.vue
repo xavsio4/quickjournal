1062d144a9bfe7adae56fe9344334e838ee2e0ff
xavier Villamuera <gzav@fifteenpeas.com>
2021-06-08 23:03:39 +0200

    - INIT

---
+++ pages/journal.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="container mx-auto">
+    <!-- the form -->
+    <div>
+      <form
+        @submit.prevent="writeToFirestore"
+        class="rounded-xl flex shadow mx-auto p-4 mt-2 mb-2 space-x-2 max-w-md"
+      >
+        {{ today }}
+        <textarea
+          v-model="what"
+          class="p-1 ml-3 shadow rounded"
+          cols="40"
+          row="2"
+          placeholder="What did you do today ?"
+          @keyup.enter="writeToFirestore"
+        ></textarea>
+        <input
+          class="
+            py-1
+            px-3
+            rounded
+            shadow
+            font-semibold
+            text-white
+            bg-green-500
+            hover:bg-green-400
+          "
+          type="submit"
+          value="Add"
+        />
+      </form>
+    </div>
+    <!-- list -->
+    <div
+      class="
+        max-w-sm
+        mx-auto
+        bg-white
+        rounded-xl
+        shadow-md
+        flex flex-row flex-initial
+        mb-1
+      "
+      v-for="(item, index) in journalEntries"
+      :key="item.id"
+      :index="index"
+    >
+      <!-- Here goes the list of done things -->
+      <div class="w-14 p-6">
+        <span class="text-gray-800">
+          {{ $moment.unix(item.data.entry_date.seconds).format('DD/MM') }}</span
+        >
+        <span class="font-light text-gray-800">
+          {{ $moment.unix(item.data.entry_date.seconds).format('YYYY') }}
+        </span>
+      </div>
+
+      <div class="text-gray-800 flex-grow p-6">
+        <span v-if="edit !== index">{{ item.data.entry }}</span>
+        <span v-if="edit === index">
+          <textarea :value="item.data.entry"></textarea>
+          <button @click="edit = null">cancel</button><button>Save</button>
+        </span>
+      </div>
+      <div class="bg-pink-200 flex-grow-0 w-9 rounded-xl rounded-l-none p-2">
+        <!-- {{ item.entry_type }} -->
+        <a class="cursor-pointer" @click="deleteEntry(item.id, index)">
+          <TrashIcon size="1.2x" class=""
+        /></a>
+        <a v-if="edit !== index" @click="editEntry(index)"
+          ><PencilIcon size="1.2x" class=""
+        /></a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import {
+  TrashIcon,
+  PencilIcon,
+  XCircleIcon,
+  BanIcon,
+  CheckIcon,
+} from '@vue-hero-icons/outline'
+
+export default {
+  components: {
+    TrashIcon,
+    PencilIcon,
+    XCircleIcon,
+    BanIcon,
+    CheckIcon,
+  },
+  data() {
+    return {
+      what: null,
+      whatedit: null,
+      ndate: new Date(),
+      undate: Math.round(new Date().getTime() / 1000),
+      journalEntries: [],
+      docEntries: [],
+      edit: null, // gets the index for edition
+    }
+  },
+  computed: {
+    today() {
+      return this.ndate.getDay() + '/' + this.ndate.getMonth()
+    },
+    year() {
+      return this.ndate.getFullYear()
+    },
+  },
+  methods: {
+    editEntry(index) {
+      this.edit = index
+    },
+    async deleteEntry(id, index) {
+      const journalRef = this.$fire.firestore.collection('journal')
+      try {
+        await journalRef.doc(id).delete()
+        this.journalEntries.splice(index, 1)
+        this.$toast.success('Entry deleted.')
+      } catch (e) {
+        this.$toast.error('Ooops, Could not delete this entry.')
+      }
+    },
+    async writeToFirestore() {
+      const journalRef = this.$fire.firestore.collection('journal')
+      // .doc('journal') // if add
+      try {
+        await journalRef.add({
+          //   set to replace and add to add
+          entry: this.what,
+          entry_date: this.ndate,
+          entry_type: 'work',
+        })
+      } catch (e) {
+        this.$toast.error('Ooops, something went wrong ...like...' + e)
+        return
+      }
+
+      this.journalEntries.unshift({
+        id: journalRef.id,
+        data: {
+          entry: this.what,
+          entry_date: { seconds: Math.round(new Date().getTime() / 1000) },
+          entry_type: 'work',
+        },
+      })
+
+      console.log(Math.round(new Date().getTime() / 1000))
+
+      this.$toast.success('Yeaaah, well done !')
+      this.what = null
+    },
+    async readFromFirestore() {
+      const journalRef = this.$fire.firestore.collection('journal')
+      // .doc('journal')
+      try {
+        const snapshot = await journalRef.get()
+        //  const doc = snapshot.data() //  for one doc
+        const doc = snapshot.docs.map((dd) => {
+          return { id: dd.id, data: dd.data() }
+        }) //   all collection
+        //  const doc2 = snapshot.docs.map((doc) => doc)
+        if (!doc) {
+          this.$toast.error('Document not there !')
+          return
+        }
+        this.journalEntries = doc
+        // this.docEntries = doc2
+        console.log(this.journalEntries)
+      } catch (e) {
+        this.$toast.error('Ooops, the app could not read a thing...' + e)
+      }
+    },
+  }, //  methods
+  mounted() {
+    this.readFromFirestore()
+  },
+}
+</script>
