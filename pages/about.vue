9da288535a7fca3df638a98ca99edbde7bbf17bc
xavier Villamuera <gzav@fifteenpeas.com>
2021-06-13 21:58:05 +0200

    added qr code

--- pages/about.vue
+++ pages/about.vue
@@ -2,14 +2,14 @@
   <div class="container">
     <div class="grid grid-cols-1 text-left">
       <h2 class="font-extrabold text-3xl">About this tiny app</h2>
-      <p>
+      <p class="mb-2">
         QuickJournal is a rapid proof of concept with a super simple purpose.
       </p>
-      <p>
+      <p class="mb-2">
         It has been built using Google's technologies. Using this service is
         done without any support from Fifteenpeas.
       </p>
-      <p>
+      <p class="mb-2">
         However, know that we are using it ourselves and that it might stay
         there and free for long.
       </p>
